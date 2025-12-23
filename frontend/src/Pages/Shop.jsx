7f7acd8d26365f5b3859ebb040e9b4ab1ecce008
Selvakrishnan k <selvakrishnan435@gmail.com>
2025-12-23 18:45:53 +0530

    Track frontend as normal folder

---
+++ frontend/src/Pages/Shop.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Hero from "../Components/Hero/Hero.jsx";
+import Popular from "../Components/Popular/Popular.jsx";
+import Offers from "../Components/Offers/Offers.jsx";
+import NewCollections from "../Components/NewCollections/NewCollections.jsx";
+
+const Shop = () => {
+    return(
+        <div>
+            <Hero/>
+            <Popular/>
+            <Offers/>
+            <NewCollections/>
+        </div>
+
+    )
+}
+export default Shop;
